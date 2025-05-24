ff13b903f0e04146d6defe011a4ac256b52fd329
VasilyYa <v.yakovina@gmail.com>
2025-05-25 01:02:25 +0500

    fix

--- lab1-script.sh
+++ lab1-script.sh
@@ -21,7 +21,7 @@ RES_HUMID=$(echo "${CONTENT}" | jq -r '.["current_condition"][0] | .humidity')
 #формируем вывод
 RES=$(echo "<!DOCTYPE html>\n<html lang=\"ru\">\n<head><meta charset=\"UTF-8\"><title>Погода</title></head>\n<body> \n")
 RES+=$(echo "Погода сегодня в $CITY: \n")
-RES+=$(echo "<ul>\n <li> температура ${RES_TEMP} °C </li>\n <li> влажность ${RES_HUMID} % </li> \n <li> время замера параметров - ${RES_TIME} </li> \n</ul> \>
+RES+=$(echo "<ul>\n <li> температура ${RES_TEMP} °C </li>\n <li> влажность ${RES_HUMID} % </li> \n <li> время замера параметров - ${RES_TIME} </li> \n</ul>")
 RES+=$(echo "Текущее время $CURR_DATE $CURR_TIME \n")
 RES+=$(echo "</body>\n</html>")
 
